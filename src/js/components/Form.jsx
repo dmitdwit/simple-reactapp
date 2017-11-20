29289cd21c8ba0bda98c464f5098b8c1ed8b488d
UserLaptop <jonathan1911255@gmail.com>
2017-11-20 22:41:03 +0700

    change something

--- src/js/components/Form.jsx
+++ src/js/components/Form.jsx
@@ -4,7 +4,7 @@ class Form extends Component {
     render() {
         return (
             <div className="container mt-10">
-                <div className="col-md-4 col-md-offset-3">
+                <div className="col-md-4 col-md-offset-4 text-center">
                     asdas
                 </div>
             </div>
